5fa073f2d26f50ff0459600611a0ee984e79c74a
spicychillisus <asherben90@gmail.com>
2024-08-16 23:03:04 +0800

    where

---
+++ index.ts
@@ -0,0 +1,23 @@
+// this file is a test file for the prisma client
+
+/* const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const post = await prisma.post.update({
+      where: { id: 1 },
+      data: { published: true },
+    })
+    console.log(post)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  }) */
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
 
 export default function Home() {
   return (
