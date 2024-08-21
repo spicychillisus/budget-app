4a6b76d0d678e82636939ab82dcff6552ec44123
spicychillisus <asherben90@gmail.com>
2024-08-22 00:18:41 +0800

    chore: Add achievement-related methods and properties to Achievement class

--- src/entities/achievement.ts
+++ src/entities/achievement.ts
@@ -5,6 +5,7 @@ export default class Achievement {
     private description: string;
     private date: string;
     private userId: number;
+    private achievementId: number;
     private username: string;
     private type: string;
 
@@ -15,5 +16,84 @@ export default class Achievement {
         this.userId = 0;
         this.username = '';
         this.type = '';
+        this.achievementId = 0;
     }
+
+    public getAchievementName(): string {
+        return this.name;
+    }
+
+    public setAchievementName(name: string): any {
+        this.name = name;
+    }
+
+    public generateAchievementId(): number {
+        const id = Math.floor(Math.random() * 1000000);
+        console.log(`generated id for achievement ${this.name}: ${id}`)
+        return this.achievementId = id;
+    }
+
+    public setAchievementDescription(description: string): any {
+        this.description = description;
+    }
+
+    public getAchievementDescription(): string {
+        return this.description;
+    }
+
+    public setAchievementDate(date: string): any {
+        this.date = date;
+    }
+
+    public getAchievementDate(): string {
+        return this.date;
+    }
+
+    public setUserId(userId: number): any {
+        this.userId = userId;
+    }
+
+    public getUserId(): number {
+        return this.userId;
+    }
+
+    public setUsername(username: string): any {
+        this.username = username;
+    }
+
+    public getUsername(): string {
+        return this.username;
+    }
+
+    public setAchievementType(type: string): any {
+        this.type = type;
+    }
+
+    public getAchievementType(): string {
+        return this.type;
+    }
+
+    public getAchievementId(): number {
+        return this.achievementId;
+    }
+
+    public setAchievementId(achievementId: number): any {
+        this.achievementId = achievementId;
+    }
+
+    public getAchievement(): any {
+        const achievement = {
+            name: this.name,
+            description: this.description,
+            date: this.date,
+            userId: this.userId,
+            achievementId: this.achievementId,
+            username: this.username,
+            type: this.type
+        }
+
+        return achievement;
+    }
+
+
 }
